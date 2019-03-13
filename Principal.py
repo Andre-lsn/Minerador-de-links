f1feee68550d70a56bc8d54d9b54def5c4e61a00
SilencerHack <48533836+SilencerHack@users.noreply.github.com>
2019-03-13 19:32:16 -0300

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -3,5 +3,6 @@ import requests
 NomeLista = []
 NomeLista +=['Https://google.com']
 for i in Lista: 
-response = requests.get(i).text
-print(response)
+  response = requests.get(i).text
+  print(response)
+  
