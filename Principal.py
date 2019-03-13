715e100bdb8ab8ac95961a1d2d9b00f8aa845c71
SilencerHack <48533836+SilencerHack@users.noreply.github.com>
2019-03-13 19:31:30 -0300

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -3,5 +3,5 @@ import requests
 NomeLista = []
 NomeLista +=['Https://google.com']
 for i in Lista: 
-response = requests.get(i).text('http://www.reddit.com/r/programming/')
+response = requests.get(i).text
 print(response)
